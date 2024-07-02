1efce33cc226324736db91b36861efeed498da37
Sathaphorn-S <sathaphorn.sinlapee@gmail.com>
2024-07-02 10:59:45 +0700

    dotenv

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require('express')
+const notFound = require('./middlewares/not-found')
 const app = express()
 
+
+app.use(notFound)
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=>console.log('Server running on',port))
